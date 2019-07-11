d7a457f828db45811a09b686595eec60880fa447
AlexeyVed <alexey.vedkal@gmail.com>
2019-07-11 07:00:50 +0300

    add dump

--- dump.sql
+++ dump.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: clockwise
+-- Host: 127.0.0.1    Database: clockwise
 -- ------------------------------------------------------
--- Server version	8.0.11
+-- Server version	8.0.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,11 +23,10 @@ DROP TABLE IF EXISTS `cities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cities` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `city` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,7 +47,7 @@ DROP TABLE IF EXISTS `clocks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `clocks` (
-  `id` varchar(45) NOT NULL,
+  `id` int(11) NOT NULL,
   `typeClock` varchar(30) NOT NULL,
   `timeRepair` int(11) NOT NULL,
   PRIMARY KEY (`id`)
@@ -61,7 +60,7 @@ CREATE TABLE `clocks` (
 
 LOCK TABLES `clocks` WRITE;
 /*!40000 ALTER TABLE `clocks` DISABLE KEYS */;
-INSERT INTO `clocks` VALUES ('1','Small',1),('2','Medium',2),('3','Big',3);
+INSERT INTO `clocks` VALUES (1,'Small',1),(2,'Medium',2),(3,'Big',3);
 /*!40000 ALTER TABLE `clocks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -74,11 +73,11 @@ DROP TABLE IF EXISTS `login`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `login` (
   `idlogin` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
   PRIMARY KEY (`idlogin`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,7 +86,7 @@ CREATE TABLE `login` (
 
 LOCK TABLES `login` WRITE;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES (1,'admin@example.com','passwordsecret');
+INSERT INTO `login` VALUES (1,'admin@example.com','passwordsecret'),(2,'user1@example.com','user1');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,6 +101,7 @@ CREATE TABLE `workers` (
   `idworker` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `city` varchar(45) NOT NULL,
+  `rating` int(1) NOT NULL,
   PRIMARY KEY (`idworker`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -112,7 +112,7 @@ CREATE TABLE `workers` (
 
 LOCK TABLES `workers` WRITE;
 /*!40000 ALTER TABLE `workers` DISABLE KEYS */;
-INSERT INTO `workers` VALUES (1,'Sergey','Uzhgorod'),(2,'Andrey','Dnipro');
+INSERT INTO `workers` VALUES (1,'Sergey','Uzhgorod',5),(2,'Andrey','Dnipro',4);
 /*!40000 ALTER TABLE `workers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -125,4 +125,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-06-14  5:05:31
+-- Dump completed on 2019-07-11  7:00:10
