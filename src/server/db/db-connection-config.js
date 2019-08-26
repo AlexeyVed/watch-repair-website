a5e7bcb168cfc86ae884c62b0016184f83356ae5
AlexeyVed <alexey.vedkal@gmail.com>
2019-08-26 03:14:02 +0300

    connection db-connection-db

--- src/server/db/db-connection-config.js
+++ src/server/db/db-connection-config.js
@@ -11,9 +11,9 @@ const mysql = new Sequelize('clockwise', 'root', 'e8zbprhH', {
   }
 })
 
-const postgresql = new Sequelize('dbn9cua6pcfq6m', 'pawllgmzgkumjo', '219fda9a0c93a8b6208af0b3a15694e3e49b12f1741feeff7300225deb0b67a2', {
+const postgresql = new Sequelize('ddhe17nk5stidt', 'cytndkmrdjztds', '4c34a00ed10369edd02a851b6200870c26ab195d470c0379d1004e908769d84d', {
   dialect: 'postgres',
-  host: 'ec2-107-20-155-148.compute-1.amazonaws.com',
+  host: 'ec2-174-129-242-183.compute-1.amazonaws.com',
   port: 5432,
   pool: {
     max: 5,
