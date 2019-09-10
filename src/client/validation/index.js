b5e0879499398bbde9ea1b635d6c1fa03f0c421a
Alexey <vedkal.alexey@gmail.com>
2019-09-10 17:15:40 +0300

    validation test, add jest

--- src/client/validation/index.js
+++ src/client/validation/index.js
@@ -17,4 +17,5 @@ export const validatePassword = (input) => (
 export const confirmPassword = (input, allInputs) => (
   input === allInputs.password ? void 0 : 'Password does not match'
 )
+
 export const required = value => value ? undefined : 'Required'
