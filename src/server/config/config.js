89cc8ebf942fd7f528d05abc7eb2f7cdc6cb8ed4
Alexey <vedkal.alexey@gmail.com>
2019-09-19 13:17:54 +0300

    refactor config-db, rebase migration files

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,17 +1,7 @@
+const db = require('../db/db-config.js')
+
 const development = {
-  db: {
-    dialect: 'mysql',
-    database: 'clockwise',
-    username: 'root',
-    password: 'e8zbprhH',
-    host: 'localhost',
-    port: 3306,
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    }
-  },
+  db: db.development,
   mail: {
     service: 'gmail',
     user: 'watches.repair.quick@gmail.com',
@@ -24,20 +14,7 @@ const development = {
 }
 
 const staging = {
-  db: {
-    dialect: 'postgres', 
-    database: 'dc8mqa68q4p4qt',
-    username: 'kxzddwqyrdjfet',
-    password: '80b1daa1d854be71ac54a1ef845e478538377dfbd1a0dcc11e11186cda639463',
-    host: 'ec2-174-129-229-106.compute-1.amazonaws.com',
-    port: 5432,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  },
+  db: db.staging,
   mail: {
     service: 'gmail',
     user: 'watches.repair.quick@gmail.com',
@@ -50,20 +27,7 @@ const staging = {
 }
 
 const production = {
-  db: {
-    dialect: 'postgres',
-    database: 'ddhe17nk5stidt',
-    username: 'cytndkmrdjztds',
-    password: '4c34a00ed10369edd02a851b6200870c26ab195d470c0379d1004e908769d84d',
-    host: 'ec2-174-129-242-183.compute-1.amazonaws.com',
-    port: 5432,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000        
-    }
-  },
+  db: db.prodution,
   mail: {
     service: 'gmail',
     user: 'watches.repair.quick@gmail.com',
