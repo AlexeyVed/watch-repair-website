a92b59bbc741341f8829ed28ebecae4e97a17076
Alexey <vedkal.alexey@gmail.com>
2019-09-19 15:16:56 +0300

    simplified config, remove unused models

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -2,43 +2,43 @@ const db = require('../db/db-config.js')
 
 const mode = process.env.NODE_ENV || 'development'
 
-const development = {
-  db: db[mode],
-  mail: {
-    service: 'gmail',
-    user: 'watches.repair.quick@gmail.com',
-    pass: 'watchrepair86',
-    from: 'watches.repair.quick@gmail.com'
+const config = {
+  development: {
+    db: db[mode],
+    mail: {
+      service: 'gmail',
+      user: 'watches.repair.quick@gmail.com',
+      pass: 'watchrepair86',
+      from: 'watches.repair.quick@gmail.com'
+    },
+    jwt: {
+      secret: 'secret_development'
+    }
   },
-  jwt: {
-    secret: 'secret_development'
+  staging: {
+    db: db[mode],
+    mail: {
+      service: 'gmail',
+      user: 'watches.repair.quick@gmail.com',
+      pass: 'watchrepair86',
+      from: 'watches.repair.quick@gmail.com'
+    },
+    jwt: {
+      secret: 'secret_test'
+    }
+  },
+  production: {
+    db: db[mode],
+    mail: {
+      service: 'gmail',
+      user: 'watches.repair.quick@gmail.com',
+      pass: 'watchrepair86',
+      from: 'watches.repair.quick@gmail.com'
+    },
+    jwt: {
+      secret: 'secret_production'
+    }
   }
 }
 
-const staging = {
-  db: db[mode],
-  mail: {
-    service: 'gmail',
-    user: 'watches.repair.quick@gmail.com',
-    pass: 'watchrepair86',
-    from: 'watches.repair.quick@gmail.com'
-  },
-  jwt: {
-    secret: 'secret_test'
-  }
-}
-
-const production = {
-  db: db[mode],
-  mail: {
-    service: 'gmail',
-    user: 'watches.repair.quick@gmail.com',
-    pass: 'watchrepair86',
-    from: 'watches.repair.quick@gmail.com'
-  },
-  jwt: {
-    secret: 'secret_production'
-  }
-}
-
-module.exports = (mode === 'poduction') ? production : (mode === 'staging') ? staging : development
+module.exports = config[mode]
