e501945d47303de75e65bec5e3c181b63781156d
Alexey <vedkal.alexey@gmail.com>
2019-09-24 14:04:02 +0300

    fix error dublicate user

--- src/server/db/seeders/20190924083137-admin.js
+++ src/server/db/seeders/20190924083137-admin.js
@@ -6,6 +6,9 @@ module.exports = {
       email: 'admin@example.com',
       password: 'passwordsecret'
     }], {})
+      .catch(() => {
+        return null
+      })
   },
 
   down: (queryInterface, Sequelize) => {
