5ff6d3de69548d3b7707f9c7967dd9b4f514159c
Alexey <vedkal.alexey@gmail.com>
2019-09-24 19:54:29 +0300

    replace flag for test to NODE_ENV

--- src/client/actions/loginActions.js
+++ src/client/actions/loginActions.js
@@ -8,7 +8,7 @@ import {
 
 import axios from 'axios'
 
-export const loginToApp = (values, test) => {
+export const loginToApp = values => {
   return (dispatch) => {
     dispatch({ type: SING_IN_STARTED })
     return axios
@@ -16,7 +16,7 @@ export const loginToApp = (values, test) => {
       .then(res => {
         localStorage.setItem('user', res.data.user.email)
         localStorage.setItem('token', res.data.token)
-        if (!test) {
+        if (process.env.NODE_ENV !== 'test') {
           axios.defaults.headers.common['authorization'] = res.data.token
         }
         dispatch(singInSuccess(res.data.user.email))
@@ -27,14 +27,14 @@ export const loginToApp = (values, test) => {
   }
 }
 
-export const logOutApp = test => {
+export const logOutApp = () => {
   return (dispatch) => {
     return axios
       .post(`/api/users/logout`)
       .then(res => {
         localStorage.removeItem('user')
         localStorage.removeItem('token')
-        if (!test) {
+        if (process.env.NODE_ENV !== 'test') {
           delete axios.defaults.headers.common['authorization']
         }
         dispatch({ type: LOG_OUT })
