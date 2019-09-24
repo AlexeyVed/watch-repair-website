c24716be92f2b2686ab0605c10bc4456babc03d1
Alexey <vedkal.alexey@gmail.com>
2019-09-24 11:16:48 +0300

    add config for heroku staging

--- src/server/models/cities.js
+++ src/server/models/cities.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const db = require('../db/db-connection-config.js')
+const db = require('../db/db-connection.js')
 
 const City = db.define('cities', {
   city: {
--- src/server/modules/middleware/isAuthenticate.js
+++ src/server/modules/middleware/isAuthenticate.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken')
+const config = require('../../config/config.js')
 
 module.exports = (req, res, next) => {
   const token = req.headers['authorization']
   try {
-    const decoded = jwt.verify(token, 'secret_word')
+    const decoded = jwt.verify(token, config.jwt.secret)
     if (decoded.email) {
       next()
     }
