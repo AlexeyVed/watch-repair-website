6ea1d63d144057747719bdef2ccf70cd2d518599
Alexey <vedkal.alexey@gmail.com>
2019-10-18 19:15:56 +0300

    add tests for cityController

--- src/server/controllers/__tests__/citiesController.test.js
+++ src/server/controllers/__tests__/citiesController.test.js
@@ -4,7 +4,7 @@ const app = require('../../app.js')
 describe('Test the cityController', () => {
   let token
 
-  test('It should request post to path = "/api/user/"', (done) => {
+  test('It should request for login, path = "/login"', (done) => {
     return request(app)
       .post('/api/users/login')
       .send({ email: 'admin@example.com', password: 'passwordsecret' })
@@ -16,7 +16,7 @@ describe('Test the cityController', () => {
       })
   })
 
-  test('It should request get to path = "/api/cities/"', (done) => {
+  test('It should request for get cities, path = "/api/cities/"', (done) => {
     return request(app)
       .get('/api/cities/')
       .then((res) => {
@@ -25,7 +25,7 @@ describe('Test the cityController', () => {
       })
   })
 
-  test('It should request post to path = "/api/cities/"', (done) => {
+  test('It should request for create city, path = "/api/cities/"', (done) => {
     return request(app)
       .post('/api/cities')
       .send({ name: 'Dnipro' })
@@ -35,15 +35,108 @@ describe('Test the cityController', () => {
         expect(JSON.parse(res.text)).toEqual({ id: 1, name: 'Dnipro' })
         done()
       })
-      // return request(app)
-      // .post('/api/cities')
-      // .send({ name: 123 })
-      // .set('Accept', 'application/json')
-      // .set('Authorization', token)
-      // .then((res) => {
-      //   expect(res.statusCode).toBe(422)
-      //   expect(JSON.parse(res.text)).toEqual('some')
-      //   done()
-      // })
+  })
+
+  test('It should request for create city with error validation, path = "/api/cities/"', (done) => {
+    return request(app)
+      .post('/api/cities')
+      .send({ name: 2 })
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual('Incorrect fields: name.')
+        done()
+      })
+  })
+
+  test('It should request for get city with error validation, path = "/api/cities/"', (done) => {
+    return request(app)
+      .get('/api/cities/null')
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual('Incorrect fields: id.')
+        done()
+      })
+  })
+
+  test('It should request for get city with error not found, path = "/api/cities/"', (done) => {
+    return request(app)
+      .get('/api/cities/2')
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual('City with id = 2 not found!')
+        done()
+      })
+  })
+
+  test('It should request for get city, path = "/api/cities/"', (done) => {
+    return request(app)
+      .get('/api/cities/1')
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual({ id: 1, name: 'Dnipro' })
+        done()
+      })
+  })
+
+  test('It should request for update city with error validation, path = "/api/cities/"', (done) => {
+    return request(app)
+      .put('/api/cities/null')
+      .send({ name: 1 })
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual('Incorrect fields: id, name.')
+        done()
+      })
+  })
+
+  test('It should request for update city with error not found, path = "/api/cities/"', (done) => {
+    return request(app)
+      .put('/api/cities/2')
+      .send({ name: 'Uzghorod' })
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual('City with id = 2 not found for update!')
+        done()
+      })
+  })
+
+  test('It should request for update city, path = "/api/cities/"', (done) => {
+    return request(app)
+      .put('/api/cities/1')
+      .send({ name: 'Uzghorod' })
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual({ id: 1, name: 'Uzghorod' })
+        done()
+      })
+  })
+
+  test('It should request for delete city with error validation, path = "/api/cities/"', (done) => {
+    return request(app)
+      .delete('/api/cities/null')
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual('Incorrect fields: id.')
+        done()
+      })
+  })
+
+  test('It should request for delete city, path = "/api/cities/"', (done) => {
+    return request(app)
+      .delete('/api/cities/1')
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .then((res) => {
+        expect(JSON.parse(res.text)).toEqual(1)
+        done()
+      })
   })
 })
